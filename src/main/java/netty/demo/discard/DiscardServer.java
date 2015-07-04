028aabe5bc7f07cd4fa197b01134d228f52999a9
niuqinghua <nqh_2005@163.com>
2015-07-04 15:58:50 +0800

    optimize demo

--- src/main/java/netty/demo/discard/DiscardServer.java
+++ src/main/java/netty/demo/discard/DiscardServer.java
@@ -4,59 +4,30 @@
 package netty.demo.discard;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import netty.demo.AbstractServer;
 
 /**
  * Created by niuqinghua on 2015/6/12.
  */
-public class DiscardServer {
-
-    private int port;
+public class DiscardServer extends AbstractServer {
 
     public DiscardServer(int port) {
-        this.port = port;
+        super(port);
     }
 
-    public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup, workerGroup)
-                                    .channel(NioServerSocketChannel.class)
-                                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                                        @Override
-                                        public void initChannel(SocketChannel socketChannel) throws Exception{
-                                            socketChannel.pipeline().addLast(new DiscardServerHandler());
-                                        }
-                                    })
-                                    .option(ChannelOption.SO_BACKLOG, 128)
-                                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
-
-            channelFuture.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+    @Override
+    protected ChannelHandler createChildChannelHandler() {
+        return new DiscardServerClildChannelHadler();
     }
 
     public static void main(String[] args) throws Exception{
-        int port;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        } else {
-            port = 8080;
-        }
-        new DiscardServer(port).run();
+        int port = args.length > 0 ? Integer.valueOf(args[0]) : 8080;
+        DiscardServer discardServer = new DiscardServer(port);
+        discardServer.run();
     }
 
 }
--- src/main/java/netty/demo/echo/EchoServer.java
+++ src/main/java/netty/demo/echo/EchoServer.java
@@ -2,41 +2,25 @@ package netty.demo.echo;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import netty.demo.AbstractServer;
 
 /**
  * Created by niuqinghua on 2015/7/4.
  */
-public class EchoServer {
-
-    private int port;
+public class EchoServer extends AbstractServer {
 
     public EchoServer(int port) {
-        this.port = port;
+        super(port);
     }
 
-    public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup, workerGroup);
-            serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.childHandler(new EchoServerChildChannelHandler());
-            serverBootstrap.option(ChannelOption.SO_BACKLOG, 128);
-            serverBootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
-
-            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
-
-            channelFuture.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
-        }
+    @Override
+    protected ChannelHandler createChildChannelHandler() {
+        return new EchoServerChildChannelHandler();
     }
 
     public static void main(String[] args) throws Exception {
