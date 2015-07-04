05c394ea2e85f929f1a2030f2a7cacff9c3dafe2
niuqinghua <nqh_2005@163.com>
2015-07-04 22:00:44 +0800

    add unsharable child handler

---
+++ src/main/java/netty/demo/time/TimeServerWithUnSharableChildHandler.java
@@ -0,0 +1,26 @@
+package netty.demo.time;
+
+import io.netty.channel.ChannelHandler;
+import netty.demo.AbstractServer;
+
+/**
+ * Created by niuqinghua on 2015/7/4.
+ */
+public class TimeServerWithUnSharableChildHandler extends AbstractServer {
+
+    public TimeServerWithUnSharableChildHandler(int port) {
+        super(port);
+    }
+
+    @Override
+    protected ChannelHandler createChildChannelHandler() {
+        return new TimeServerUnsharableChildHandler();
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = args.length > 0 ? Integer.valueOf(args[0]) : 8080;
+        TimeServerWithUnSharableChildHandler echoServer = new TimeServerWithUnSharableChildHandler(port);
+        echoServer.run();
+    }
+
+}
