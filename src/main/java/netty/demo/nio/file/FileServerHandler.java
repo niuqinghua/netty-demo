a8439013cd1ba573de118abc2d7d3e82d31dc581
niuqinghua <nqh_2005@163.com>
2015-07-12 00:03:05 +0800

    add FileChannel related demo

---
+++ src/main/java/netty/demo/nio/file/FileServerHandler.java
@@ -0,0 +1,47 @@
+package netty.demo.nio.file;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.DefaultFileRegion;
+import io.netty.channel.FileRegion;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by niuqinghua on 2015/7/11.
+ */
+public class FileServerHandler extends SimpleChannelInboundHandler<String> {
+
+    private static final String CR = System.getProperty("line.separator");
+
+    @Override
+    protected void messageReceived(ChannelHandlerContext ctx, String msg) throws Exception {
+        File file = new File(msg);
+
+        if (!file.exists()) {
+            ctx.writeAndFlush("File not found: " + file + CR);
+            return;
+        }
+
+        if(!file.isFile()) {
+            ctx.writeAndFlush("Not a file: " + file + CR);
+            return;
+        }
+
+        ctx.writeAndFlush(file + " " + file.length() + CR);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(msg, "r");
+        FileChannel fileChannel = randomAccessFile.getChannel();
+        FileRegion region = new DefaultFileRegion(fileChannel, 0, randomAccessFile.length());
+        ctx.write(region);
+        ctx.writeAndFlush(CR);
+        randomAccessFile.close();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+
+}
--- src/main/java/netty/demo/nio/file/NioTcpFileServer.java
+++ src/main/java/netty/demo/nio/file/NioTcpFileServer.java
@@ -1,12 +1,23 @@
 package netty.demo.nio.file;
 
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
 import netty.demo.nio.AbstractNioTcpServer;
+
+import java.nio.charset.Charset;
 
 /**
  * Created by niuqinghua on 2015/7/11.
  */
 public class NioTcpFileServer extends AbstractNioTcpServer {
+
+    private static final Charset UTF_8 = CharsetUtil.UTF_8;
 
     public NioTcpFileServer(int port) {
         super(port);
@@ -14,7 +25,22 @@ public class NioTcpFileServer extends AbstractNioTcpServer {
 
     @Override
     protected ChannelHandler createChildChannelHandler() {
-        return null;
+        return new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(new StringEncoder(UTF_8));
+                pipeline.addLast(new LineBasedFrameDecoder(1024));
+                pipeline.addLast(new StringDecoder(UTF_8));
+                pipeline.addLast(new FileServerHandler());
+            }
+        };
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = args.length > 0 ? Integer.valueOf(args[0]) : 8080;
+        NioTcpFileServer server = new NioTcpFileServer(port);
+        server.run();
     }
 
 }
