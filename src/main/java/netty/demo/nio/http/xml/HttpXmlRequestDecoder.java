b9e3614ecc6bdb70977de07afc8883d2a99fc609
niuqinghua <nqh_2005@163.com>
2015-07-14 00:56:52 +0800

    modify http request decoder and encoder

--- src/main/java/netty/demo/nio/http/xml/HttpXmlRequestDecoder.java
+++ src/main/java/netty/demo/nio/http/xml/HttpXmlRequestDecoder.java
@@ -1,10 +1,19 @@
 package netty.demo.nio.http.xml;
 
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpResponseStatus;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.CharsetUtil;
 
 /**
  * Created by niuqinghua on 2015/7/13.
@@ -22,7 +31,13 @@ public class HttpXmlRequestDecoder extends AbstractHttpXmlDecoder<FullHttpReques
     }
 
     private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, status, getContent(status));
+        response.headers().set(CONTENT_TYPE, "text/plain;charset=UTF-8");
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
 
+    private static ByteBuf getContent(HttpResponseStatus status) {
+        return Unpooled.copiedBuffer("Failure: " + status.toString() + "\r\n", CharsetUtil.UTF_8);
     }
 
 }
