d505d44f3b96b4b7e76ad4924a0b11f5a0c0c007
niuqinghua <nqh_2005@163.com>
2015-07-14 00:19:07 +0800

    modify http response decoder and encoder

--- src/main/java/netty/demo/nio/http/xml/HttpXmlResponseDecoder.java
+++ src/main/java/netty/demo/nio/http/xml/HttpXmlResponseDecoder.java
@@ -1,6 +1,7 @@
 package netty.demo.nio.http.xml;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 
 import java.util.List;
@@ -8,11 +9,12 @@ import java.util.List;
 /**
  * Created by niuqinghua on 2015/7/13.
  */
-public class HttpXmlResponseDecoder extends AbstractHttpXmlDecoder<FullHttpRequest> {
+public class HttpXmlResponseDecoder extends AbstractHttpXmlDecoder<DefaultFullHttpResponse> {
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, FullHttpRequest msg, List<Object> out) throws Exception {
-
+    protected void decode(ChannelHandlerContext ctx, DefaultFullHttpResponse msg, List<Object> out) throws Exception {
+        HttpXmlResponse response = new HttpXmlResponse(msg, decode0(ctx, msg.content(), DefaultFullHttpResponse.class));
+        out.add(response);
     }
 
 }
